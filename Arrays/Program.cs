bbd26dd50829572022d4e03bf1d52dd78729d1ac
Carlos Miguel Monteiro <c.b.monteiro@hotmail.com>
2025-03-23 01:55:17 +0000

    Testing quicksort

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -15,5 +15,60 @@ static int[] QuickSort(int[] array)
 
     var pivot = array[^1];
 
+    var leftSize = 0;
 
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] <= pivot)
+        {
+            leftSize++;
+        }
+    }
+
+    var leftPartition = new int[leftSize];
+    var rightPartition = new int[array.Length - leftSize];
+    var counter = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] <= pivot)
+        {
+            leftPartition[counter] = array[i];
+            counter++;
+        }
+    }
+
+    counter = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > pivot)
+        {
+            rightPartition[counter] = array[i];
+            counter++;
+        }
+    }
+
+    var leftSorted = QuickSort(leftPartition);
+    var rightSorted = QuickSort(rightPartition);
+
+    var result = new int[array.Length];
+
+    counter = 0;
+
+    for (int i = 0; i < leftSorted.Length; i++)
+    {
+        result[counter] = leftSorted[i];
+        counter++;
+    }
+
+    result[counter] = pivot;
+
+    for (int i = 0; i < rightSorted.Length; i++)
+    {
+        result[counter] = leftSorted[i];
+        counter++;
+    }
+
+    return QuickSort(result);
 }
