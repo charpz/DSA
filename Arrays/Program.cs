ecc7081c06f71bb328c779eea7e8354461f9183e
Carlos Miguel Monteiro <c.b.monteiro@hotmail.com>
2025-03-23 02:45:32 +0000

    Working QuickSort algorithm

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -3,72 +3,37 @@
 int[] sampleArray = [11, 9, 12, 7, 3];
 
 Console.Write("QuickSorted array: ");
-var selectSorted = QuickSort(sampleArray);
-foreach (var item in selectSorted)
+QuickSort(sampleArray, 0, sampleArray.Length - 1);
+foreach (var item in sampleArray)
 {
     Console.Write(item + " ");
 }
 
-static int[] QuickSort(int[] array)
+static void QuickSort(int[] array, int lowIndex, int highIndex)
 {
-    if (array.Length <= 1) return array;
+    if (lowIndex >= highIndex) return;
 
-    var pivot = array[^1];
+    var pivot = array[highIndex];
+    var leftPointer = lowIndex;
+    var rightPointer = highIndex;
 
-    var leftSize = 0;
-
-    for (int i = 0; i < array.Length; i++)
+    while (leftPointer < rightPointer)
     {
-        if (array[i] <= pivot)
+        while (array[leftPointer] <= pivot && leftPointer < rightPointer)
         {
-            leftSize++;
+            leftPointer++;
         }
-    }
 
-    var leftPartition = new int[leftSize];
-    var rightPartition = new int[array.Length - leftSize];
-    var counter = 0;
-
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (array[i] <= pivot)
+        while (array[rightPointer] >= pivot && leftPointer < rightPointer)
         {
-            leftPartition[counter] = array[i];
-            counter++;
+            rightPointer--;
         }
+
+        (array[rightPointer], array[leftPointer]) = (array[leftPointer], array[rightPointer]);
     }
 
-    counter = 0;
+    (array[highIndex], array[leftPointer]) = (array[leftPointer], array[highIndex]);
 
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (array[i] > pivot)
-        {
-            rightPartition[counter] = array[i];
-            counter++;
-        }
-    }
-
-    var leftSorted = QuickSort(leftPartition);
-    var rightSorted = QuickSort(rightPartition);
-
-    var result = new int[array.Length];
-
-    counter = 0;
-
-    for (int i = 0; i < leftSorted.Length; i++)
-    {
-        result[counter] = leftSorted[i];
-        counter++;
-    }
-
-    result[counter] = pivot;
-
-    for (int i = 0; i < rightSorted.Length; i++)
-    {
-        result[counter] = leftSorted[i];
-        counter++;
-    }
-
-    return QuickSort(result);
+    QuickSort(array, lowIndex, leftPointer - 1);
+    QuickSort(array, leftPointer + 1, highIndex);
 }
