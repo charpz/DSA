fd4e5672d4b1b02efbc0b21ba87a895f9447ea05
Carlos Miguel Monteiro <c.b.monteiro@hotmail.com>
2025-03-25 00:27:52 +0000

    Add unit test project

---
+++ UnitTests/ArrayAlgorithms_should.cs
@@ -0,0 +1,18 @@
+﻿using Arrays;
+
+namespace UnitTests;
+
+public class ArrayAlgorithms_should
+{
+    private const int Min = 1;
+    private const int Max = 1000;
+    private readonly int[] SampleArray = [12, 43, 3, 123, 345, 234, Min, 678, 34, Max, 4, 3, 67, 8, 69];
+
+    [Fact]
+    public void Find_lowest_value_in_array()
+    {
+        var result = ArrayAlgorithms.FindLowestValueInArray(SampleArray);
+
+        Assert.Equal(Min, result);
+    }
+}
